926587b73ca206a42a6d24aa2147c2b739e73faf
Dillion Verma <dillionverma@hotmail.com>
2017-12-02 00:16:33 -0500

    add instructions

--- GraphicBoard.cc
+++ GraphicBoard.cc
@@ -6,10 +6,14 @@ using namespace std;
 
 GraphicBoard::GraphicBoard(int winSize):winSize{winSize}, xw{winSize, winSize}{
     xw.fillRectangle(0, 0, winSize, winSize, Xwindow::White);
+    // for now just figure out which pixels will go where,
+    // what width and height etc.
+    // Define constants and use functions for "card" and "player"
 }
 
 
 void GraphicBoard::notify(Player &p) {
-  
+  // later on, when observer patter done, we will have display items on notify
+  // i.e move everything from constructor to here
 }
 
