f92edbc54bd2b9ad0921dd2cf8ae1731f337475c
Dillion Verma <dillionverma@hotmail.com>
2017-12-03 17:45:46 -0500

    setup layout of board

--- GraphicBoard.cc
+++ GraphicBoard.cc
@@ -7,7 +7,7 @@ using namespace std;
 
 GraphicBoard::GraphicBoard(int winSize):winSize{winSize}, xw{winSize, winSize}{
     int w = winSize;
-    int h = winSize;
+    //int h = winSize;
     int mid = winSize/2;
     int btw = 5;
     int bth = 20;
@@ -15,8 +15,8 @@ GraphicBoard::GraphicBoard(int winSize):winSize{winSize}, xw{winSize, winSize}{
     int ch = 100;
     int s = winSize/5;
     //enum {White=0, Black, Red, Green, Blue, Cyan, Yellow, Magenta, Orange, Brown}; // Available colours.
-    xw.fillRectangle(0, 0, winSize, winSize, Xwindow::Yellow);
-    //xw.fillRectangle(btw, bth, winSize-5-btw, winSize-5-bth, Xwindow::Yellow);
+    xw.fillRectangle(0, 0, winSize, winSize, Xwindow::Brown);
+    xw.fillRectangle(15, 15, w-15*2, bth + ch*5 + 15, Xwindow::Yellow);
 
     for (int i = 0; i < 5; ++i) {
       xw.fillRectangle(s*i + 20, bth, cw, ch, Xwindow::Green);
@@ -25,21 +25,19 @@ GraphicBoard::GraphicBoard(int winSize):winSize{winSize}, xw{winSize, winSize}{
       xw.fillRectangle(s*i + 20, bth + ch + 20, cw, ch, Xwindow::Blue);
     }
 
-    xw.drawBigString(winSize/2-80, bth + ch*2 + 20*2 + 35,"Sorcery", Xwindow::Black);
+    xw.drawBigString(mid-80, bth + ch*2 + 20*2 + 35,"Sorcery", Xwindow::Black);
 
     for (int i = 0; i < 5; ++i) {
       xw.fillRectangle(s*i + 20, bth + ch*3, cw, ch, Xwindow::Blue);
     }
 
     for (int i = 0; i < 5; ++i) {
-      xw.fillRectangle(s*i + 20, bth + ch*4 + 40, cw, ch, Xwindow::Green);
+      xw.fillRectangle(s*i + 20, bth + ch*4 + 20, cw, ch, Xwindow::Green);
     }
-    //for (int i = 0; i < 5; ++i) {
-      //xw.fillRectangle(5 + btw + i*(cw+20), bth, cw, ch, Xwindow::Green);
-    //}
-    //for (int i = 0; i < 5; ++i) {
-      //xw.fillRectangle(5 + btw + i*(cw+20), bth, cw, ch, Xwindow::Green);
-    //}
+
+    for (int i = 0; i < 5; ++i) {
+      xw.fillRectangle(s*i + 20, bth + ch*5 + 60, cw, ch, Xwindow::Blue);
+    }
     // for now just figure out which pixels will go where,
     // what width and height etc.
     // Define constants and use functions for "card" and "player"
